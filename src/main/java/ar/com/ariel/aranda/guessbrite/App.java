04f1135d660f32b9ed5a93e9ab94e87a311c4398
Ariel <aranda.ariel.norberto@gmail.com>
2018-05-20 08:54:55 -0300

    first commit - empty maven project

---
+++ src/main/java/ar/com/ariel/aranda/guessbrite/App.java
@@ -0,0 +1,13 @@
+package ar.com.ariel.aranda.guessbrite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
